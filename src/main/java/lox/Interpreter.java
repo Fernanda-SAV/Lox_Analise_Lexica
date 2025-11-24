6a340c871cf07a4a1be7a09caff02dda54526980
izadora-paula <izadora.paula@discente.ufma.br>
2025-11-24 16:14:31 -0300

    Implementação até o item 7.2.3, avaliação de expressões unárias.

---
+++ src/main/java/lox/Interpreter.java
@@ -0,0 +1,31 @@
+package main.java.lox;
+
+abstract class Interpreter implements Expr.Visitor<Object> {
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double)right;
+        }
+
+        // Unreachable.
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+}
