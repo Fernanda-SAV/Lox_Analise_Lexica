fe9b5cda4314b66a6f4122fd45edb8c99ff31e27
izadora-paula <izadora.paula@discente.ufma.br>
2025-12-03 14:09:16 -0300

    Finalização do capítulo 8, com a implementação dos itens 8.3 a 8.5

--- src/main/java/lox/Interpreter.java
+++ src/main/java/lox/Interpreter.java
@@ -2,7 +2,7 @@ package main.java.lox;
 import java.util.List;
  class Interpreter implements Expr.Visitor<Object>,
          Stmt.Visitor<Void>{
-
+     private Environment environment = new Environment();
      void interpret(List<Stmt> statements) {
          try {
              for (Stmt statement : statements) {
@@ -22,6 +22,22 @@ import java.util.List;
          Object value = evaluate(stmt.expression);
          System.out.println(stringify(value));
          return null;
+     }
+     @Override
+     public Void visitVarStmt(Stmt.Var stmt) {
+         Object value = null;
+         if (stmt.initializer != null) {
+             value = evaluate(stmt.initializer);
+         }
+
+         environment.define(stmt.name.lexeme, value);
+         return null;
+     }
+     @Override
+     public Object visitAssignExpr(Expr.Assign expr) {
+         Object value = evaluate(expr.value);
+         environment.assign(expr.name, value);
+         return value;
      }
 
     @Override
@@ -43,6 +59,10 @@ import java.util.List;
         // Unreachable.
         return null;
     }
+     @Override
+     public Object visitVariableExpr(Expr.Variable expr) {
+         return environment.get(expr.name);
+     }
     private void checkNumberOperand(Token operator, Object operand) {
         if (operand instanceof Double) return;
         throw new RuntimeError(operator, "Operand must be a number.");
@@ -69,6 +89,24 @@ import java.util.List;
     private void execute(Stmt stmt) {
          stmt.accept(this);
     }
+     @Override
+     public Void visitBlockStmt(Stmt.Block stmt) {
+         executeBlock(stmt.statements, new Environment(environment));
+         return null;
+     }
+     void executeBlock(List<Stmt> statements,
+                       Environment environment) {
+         Environment previous = this.environment;
+         try {
+             this.environment = environment;
+
+             for (Stmt statement : statements) {
+                 execute(statement);
+             }
+         } finally {
+             this.environment = previous;
+         }
+     }
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
         Object left = evaluate(expr.left);
