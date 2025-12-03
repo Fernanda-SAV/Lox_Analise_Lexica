48767a883afddca89c5a1c3537c12888aecb8714
Fernanda-SAV <fernanda.sav@discente.ufma.br>
2025-12-03 15:46:47 -0300

    atualizando com o topico 9.4 e 9.5

--- src/main/java/lox/Interpreter.java
+++ src/main/java/lox/Interpreter.java
@@ -44,6 +44,15 @@ import java.util.List;
          environment.define(stmt.name.lexeme, value);
          return null;
      }
+
+     @Override
+     public Void visitWhileStmt(Stmt.While stmt) {
+         while (isTruthy(evaluate(stmt.condition))) {
+             execute(stmt.body);
+         }
+         return null;
+     }
+
      @Override
      public Object visitAssignExpr(Expr.Assign expr) {
          Object value = evaluate(expr.value);
@@ -55,6 +64,20 @@ import java.util.List;
     public Object visitLiteralExpr(Expr.Literal expr) {
         return expr.value;
     }
+
+     @Override
+     public Object visitLogicalExpr(Expr.Logical expr) {
+         Object left = evaluate(expr.left);
+
+         if (expr.operator.type == TokenType.OR) {
+             if (isTruthy(left)) return left;
+         } else {
+             if (!isTruthy(left)) return left;
+         }
+
+         return evaluate(expr.right);
+     }
+
     @Override
     public Object visitUnaryExpr(Expr.Unary expr) {
         Object right = evaluate(expr.right);
