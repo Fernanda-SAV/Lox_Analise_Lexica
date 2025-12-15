a3de0affbf9a40fde454c8bec0405045c74ca7a7
Fernanda-SAV <fernanda.sav@discente.ufma.br>
2025-12-15 13:54:31 -0300

    atualizando o Interpreter.java, o LoxInstance.java, o LoxClass.java e o Resolver.java

--- src/main/java/lox/Resolver.java
+++ src/main/java/lox/Resolver.java
@@ -17,7 +17,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     private enum FunctionType {
         NONE,
-        FUNCTION
+        FUNCTION,
+        METHOD
     }
 
     public Void visitBlockStmt(Stmt.Block stmt) {
@@ -31,8 +32,15 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitClassStmt(Stmt.Class stmt) {
         declare(stmt.name);
         define(stmt.name);
+
+        for (Stmt.Function method : stmt.methods) {
+            FunctionType declaration = FunctionType.METHOD;
+            resolveFunction(method, declaration);
+        }
+
         return null;
     }
+
 
     @Override
     public Void visitExpressionStmt(Stmt.Expression stmt) {
