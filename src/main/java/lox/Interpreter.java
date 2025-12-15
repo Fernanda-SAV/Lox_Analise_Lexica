9fbbb79e49a16377ea10dca2ab8d85c3099072e7
Fernanda-SAV <fernanda.sav@discente.ufma.br>
2025-12-15 13:15:58 -0300

    atualizando com o topico 11.4 a classe Interpreter.java

--- src/main/java/lox/Interpreter.java
+++ src/main/java/lox/Interpreter.java
@@ -90,7 +90,12 @@ import java.util.Map;
      @Override
      public Object visitAssignExpr(Expr.Assign expr) {
          Object value = evaluate(expr.value);
-         environment.assign(expr.name, value);
+         Integer distance = locals.get(expr);
+         if (distance != null) {
+             environment.assignAt(distance, expr.name, value);
+         } else {
+             globals.assign(expr.name, value);
+         }
          return value;
      }
 
